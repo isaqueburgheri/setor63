2dd54beb66a9e3b7872040175d3bc116a11421cb
isaqueburgheri <isaqueburgheri@gmail.com>
2023-08-14 13:46:26 -0300

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // Você pode criar um arquivo CSS para estilização
-import Home from './Home'; // Importe o componente Home que você mencionou
+import './index.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <header>
+        <h1>Bem-vindo ao Nosso Site Religioso</h1>
+      </header>
+      <main>
+        <section className="about">
+          <h2>Sobre Nós</h2>
+          <p>
+            Somos uma comunidade dedicada à fé e espiritualidade, buscando inspiração e orientação em nossa jornada espiritual.
+          </p>
+        </section>
+        <section className="teachings">
+          <h2>Ensinamentos</h2>
+          <p>
+            Nossos ensinamentos são baseados em valores e princípios fundamentais que nos guiam na busca por uma vida significativa e conectada com o divino.
+          </p>
+        </section>
+        <section className="events">
+          <h2>Eventos</h2>
+          <p>
+            Junte-se a nós em nossos eventos e cerimônias para compartilhar momentos de oração, reflexão e comunhão.
+          </p>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; {new Date().getFullYear()} Nosso Site Religioso. Todos os direitos reservados.</p>
+      </footer>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,6 +41,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-export default Home
